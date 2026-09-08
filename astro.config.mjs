461ff4c0226db26ec671d0b4c7d0be52f4fe1afa
Nirbhay Kwatra <kwatranirbhay@gmail.com>
2026-09-08 16:04:05 -0700

    Add Astro integrations
    - Added React, Markdoc, MDX, Tailwind and SEO integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,32 +4,50 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
+import node from '@astrojs/node';
+
+import react from '@astrojs/react';
+
+import markdoc from '@astrojs/markdoc';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+  site: 'https://portfolio.nirbhaykwatra.com',
+  output: 'static',
+  integrations: [mdx(), sitemap(), react(), markdoc()],
+
+  fonts: [
+      {
+          provider: fontProviders.local(),
+          name: 'Atkinson',
+          cssVariable: '--font-atkinson',
+          fallbacks: ['sans-serif'],
+          options: {
+              variants: [
+                  {
+                      src: ['./src/assets/fonts/atkinson-regular.woff'],
+                      weight: 400,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+                  {
+                      src: ['./src/assets/fonts/atkinson-bold.woff'],
+                      weight: 700,
+                      style: 'normal',
+                      display: 'swap',
+                  },
+              ],
+          },
+      },
+    ],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
